45954399435dead666678fb58d5fac9520b7c6f2
R221 <rcjames1111@gmail.com>
2024-10-27 21:19:51 -0500

    update output target build

--- astro.config.ts
+++ astro.config.ts
@@ -5,6 +5,7 @@ import UnoCSS from 'unocss/astro';
 export default defineConfig({
   // used to generate images
   site: 'https://vul.ninja',
+  output: 'static',
 
   trailingSlash: 'ignore',
   integrations: [sitemap(), UnoCSS({ injectReset: true })],
